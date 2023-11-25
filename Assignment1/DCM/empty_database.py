d2bdd117b9f2e934fcd410a99c53728d69406a09
ConnorUsaty <connorusaty@gmail.com>
2023-11-24 23:04:00 -0500

    Create empty_database.py
    Test function that empties database except for admin account

---
+++ Assignment1/DCM/empty_database.py
@@ -0,0 +1,23 @@
+from sqlite3 import connect
+
+# Empty the database except admin user
+# Used for testing purposes
+def empty_database():
+    conn = connect('./users.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM all_users')
+    users = c.fetchall()
+    for i in range (2, len(users)+1):
+        id = i
+        c.execute('DELETE FROM all_users WHERE id = ?', (id,))
+        c.execute('DELETE FROM AOO_data WHERE id = ?', (id,))
+        c.execute('DELETE FROM VOO_data WHERE id = ?', (id,))
+        c.execute('DELETE FROM AAI_data WHERE id = ?', (id,))
+        c.execute('DELETE FROM VVI_data WHERE id = ?', (id,))
+        c.execute('DELETE FROM AOOR_data WHERE id = ?', (id,))
+        c.execute('DELETE FROM VOOR_data WHERE id = ?', (id,))
+        c.execute('DELETE FROM AAIR_data WHERE id = ?', (id,))
+        c.execute('DELETE FROM VVIR_data WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    
