88296527ba6f87d4b7b671335c3493f27b9cebb1
ConnorUsaty <connorusaty@gmail.com>
2023-11-24 23:29:05 -0500

    Imported test functions

--- Assignment1/DCM/main.py
+++ Assignment1/DCM/main.py
@@ -7,6 +7,9 @@ from hashlib import sha256
 
 from windows.welcome.welcome import MyWindow
 
+from fill_database import fill_database
+from empty_database import empty_database
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv) # create application
@@ -16,6 +19,10 @@ if __name__ == '__main__':
     stacked_window.setFixedWidth(1200) # set fixed width and height
     stacked_window.setFixedHeight(800)
     stacked_window.show()
+
+    # TEST FUNCTIONS
+    # empty_database() # empty database except for admin user
+    # fill_database() # fill database
 
 
     sys.exit(app.exec_()) 
